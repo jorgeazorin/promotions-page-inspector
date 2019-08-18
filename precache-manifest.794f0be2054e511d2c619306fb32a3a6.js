0171b330c0fe204d7dc38f009cfe76861fe65453
jorgeazorin <jorgeazorin@gmail.com>
2019-08-18 21:44:41 +0100

    Updates

---
+++ precache-manifest.794f0be2054e511d2c619306fb32a3a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badfdb0005797dcf21823c2a73b3a0ed",
+    "url": "/promotions-page-inspector/index.html"
+  },
+  {
+    "revision": "ba099cb1aeedf754a795",
+    "url": "/promotions-page-inspector/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "01fa7af3b68b94f130fe",
+    "url": "/promotions-page-inspector/static/js/2.f32952a5.chunk.js"
+  },
+  {
+    "revision": "ba099cb1aeedf754a795",
+    "url": "/promotions-page-inspector/static/js/main.4d2ce32e.chunk.js"
+  },
+  {
+    "revision": "880788a404e376a04496",
+    "url": "/promotions-page-inspector/static/js/runtime~main.c53e9004.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/promotions-page-inspector/static/media/logo.5d5d9eef.svg"
+  }
+]);
